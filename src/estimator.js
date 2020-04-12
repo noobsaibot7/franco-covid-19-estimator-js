559f9b33333db8a045d00c1e7b45361b16ec4dc6
noobsaibot7 <richysolnz@gmail.com>
2020-04-12 14:13:31 +0100

    removed commented codes

--- src/estimator.js
+++ src/estimator.js
@@ -7,27 +7,7 @@ import {
   predictInfectedRequestedVentilators,
   predictDollarsInFlight
 } from './calcs';
-/*
-{
-region: {
-name: "Africa",
-avgAge: 19.7,
-avgDailyIncomeInUSD: 5,
-avgDailyIncomePopulation: 0.71
-},
-periodType: "days",
-timeToElapse: 58,
-reportedCases: 674,
-population: 66622705,
-totalHospitalBeds: 1380614
-}
-===================================================
-{
- data: {}, // the input data you got
- impact: {}, // your best case estimation
- severeImpact: {} // your severe case estimation
-}
-*/
+
 
 const covid19ImpactEstimator = (data) => {
   const currentlyInfected = predictCurrentlyInfected(data);
@@ -44,7 +24,7 @@ const covid19ImpactEstimator = (data) => {
   const infectedRequestedVentilators = predictInfectedRequestedVentilators(
     casesForICUByRequestedTime
   );
-  predictDollarsInFlight(infectedRequestedVentilators);
+  return predictDollarsInFlight(infectedRequestedVentilators);
 };
 
 export default covid19ImpactEstimator;
